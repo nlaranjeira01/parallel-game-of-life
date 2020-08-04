6329e05dd2d88de9a3da073d2ab368ea21b07539
rockerbacon <vitor@librem.one>
2020-08-03 22:46:37 -0300

    Added script to compare sequential and parallel results

--- Makefile
+++ Makefile
@@ -8,7 +8,8 @@ LINK     =  $(LIBDIR) $(LIBS)
 CPU_COMPILE_C  = $(CPUC) $(DEFS) $(INCLUDES) $(CFLAGS)
 
 all:
-	$(CPU_COMPILE_C) main-gol.c  $(LINK)   -o gol
+	$(CPU_COMPILE_C) main-gol.c  $(LINK) -o gol
+	$(CPU_COMPILE_C) main-gol-parallel.c $(LINK) -o gol-parallel -fopenmp
 
 clean:
 	rm gol
---
+++ compare.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+args="128 128 1000 1 0.1"
+
+make
+
+./gol $args
+./gol-parallel $args
+
+echo
+
+cmp game_of_life.txt game_of_life_parallel.txt
+exit_status=$?
+
+if [ "$exit_status" == "0" ]; then
+	echo "PROGRAM WORKING CORRECTLY"
+else
+	echo "FILES ARE DIFFERENT!!!!!!"
+fi
+
+exit $exit_status
+
